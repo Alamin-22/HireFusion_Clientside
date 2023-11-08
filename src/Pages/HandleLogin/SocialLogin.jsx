faca70cdc05e151290b07928696ef8b968bab1cd
Alamin-22 <mdalaminmollick@gmail.com>
2023-11-08 15:18:17 +0600

    Fix Google login token Bug And Token Is now set successfully

--- src/Pages/HandleLogin/SocialLogin.jsx
+++ src/Pages/HandleLogin/SocialLogin.jsx
@@ -3,17 +3,32 @@ import { AiOutlineGithub } from 'react-icons/ai';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import useAuth from '../../Hooks/useAuth';
+import useAxios from '../../Hooks/useAxios';
 const SocialLogin = () => {
     const { GoogleSingIn } = useAuth();
-    
+    const axios = useAxios();
+
     const location = useLocation();
     const navigate = useNavigate();
     const handleSocialLogin = (media) => {
         media()
             .then(res => {
-                console.log(res.user)
+                console.log(res.user);
                 Swal.fire("Good job!", "Login Successful!", "success");
-                navigate(location?.state ? location.state : '/');
+                // send cookie to the server
+                const email = res.user.email;
+                const user = { email }
+                axios.post("/jwt", user)
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.success) {
+                            navigate(location?.state ? location.state : '/');
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+
             })
             .catch(error => {
                 console.log(error)
